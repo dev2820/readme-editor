fc18f5170e85071f7372244f7d8f8068926565ec
dev2820 <second28200@naver.com>
2024-03-09 17:21:34 +0900

    feat: 마크다운 추출 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { useRef } from 'react';
+
 import { BlockEditor } from './components/BlockEditor';
 import { Button } from './components/ui/Button';
 
 function App() {
+  const editorRef = useRef(null);
+  async function handleExtractMarkdown() {
+    console.log(await editorRef.current.getMarkdown());
+  }
   return (
     <>
       <main className="w-full">
         <div className="flex flex-row justify-center overflow-hidden">
           <div className="w-200 h-160 overflow-auto">
-            <BlockEditor></BlockEditor>
+            <BlockEditor ref={editorRef}></BlockEditor>
           </div>
         </div>
-        <Button>Hello World</Button>
+        <Button onClick={handleExtractMarkdown}>Hello World</Button>
       </main>
     </>
   );
