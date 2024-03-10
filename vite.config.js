684420cd36d53ee03d18edc653c19c397c1f8758
dev2820 <second28200@naver.com>
2024-03-10 14:01:41 +0900

    chore: 배포 설정 (gh-pages)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import { defineConfig } from "vite";
-
-import UnoCSS from "unocss/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import UnoCSS from 'unocss/vite';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,7 +11,8 @@ export default defineConfig({
   plugins: [react(), UnoCSS()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  base: './',
 });
