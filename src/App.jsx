a1b356318145a15c7dbf3d6b6be87bda9b6298b8
dev2820 <second28200@naver.com>
2024-03-31 18:05:11 +0900

    feat: 공사중 문구 추가

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,9 @@ function App() {
           {/* <div className="flex flex-row justify-center overflow-hidden">
             <BlockEditor className="w-200 h-160 overflow-auto"></BlockEditor>
           </div> */}
+          <p className="text-center">
+            공사중입니다. 기능이 제대로 동작하지 않을 가능성이 커요
+          </p>
           <div className="flex flex-row justify-center overflow-hidden ">
             <TestEditor className="w-150 border-1"></TestEditor>
           </div>
